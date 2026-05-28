5d8e9337d9a78e3b8ea4f33923b0ada65a23d989
du than <xopxop.1403@gmail.com>
2026-05-28 17:05:00 +0300

    test open file

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: dthan <dthan@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/03 15:35:38 by dthan             #+#    #+#             */
-/*   Updated: 2026/05/28 17:02:23 by dthan            ###   ########.fr       */
+/*   Updated: 2026/05/28 17:03:37 by dthan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "error.h"
+#include <fcntl.h>
 
 // void run(t_map *map) {
 	
@@ -21,9 +22,9 @@ int main(int ac, char **av)
 	if (ac != 2)
 		return print_error_and_exit("Usage: ./fdf <filename>");
 	
-	// int fd = open(av[1], O_RDONLY);
-	// if (fd < 0)
-	// 	return print_error_and_exit("Error: Could not open file");
+	int fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		return print_error_and_exit("Error: Could not open file");
 	// char **lines = get_lines(fd);
 	// close(fd);
 	// if (lines == 0)
